4503b412be32267d5bf4dc614cda7f33e94ba1b4
Simon Bjerre <bjerrecs@protonmail.com>
2025-04-28 08:45:10 +0200

    added skeleton for the Annoucement

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+
 async function PullForumData(URL: string) {
   try {
     const response = await fetch(URL);
@@ -14,21 +16,24 @@ async function PullForumData(URL: string) {
   }
 }
 
-
 async function AnnoucementPosts() {
-  var posts = [];
+  const posts = [];
+  const announcements = await PullForumData(
+    "https://forum.vatsim-scandinavia.org/api/discussions?filter[tag]=announcements"
+  );
 
-  const announcements = await PullForumData('https://forum.vatsim-scandinavia.org/api/discussions?filter[tag]=announcements');
-
-  announcements.sort((a: any, b:any) => {
+  announcements.sort((a: any, b: any) => {
     const dateA = new Date(a.attributes.createdAt);
     const dateB = new Date(b.attributes.createdAt);
     return dateB.getTime() - dateA.getTime();
   });
 
-  for (const post of await announcements) {
-    const firstPost = await PullForumData('https://forum.vatsim-scandinavia.org/api/posts?filter[id]=' + post.relationships.firstPost.data.id);
-    const firstPostContent = await firstPost[0].attributes.contentHtml;
+  for (const post of announcements) {
+    const firstPost = await PullForumData(
+      "https://forum.vatsim-scandinavia.org/api/posts?filter[id]=" +
+        post.relationships.firstPost.data.id
+    );
+    const firstPostContent = firstPost[0].attributes.contentHtml;
     const postdata = {
       slug: post.attributes.slug,
       title: post.attributes.title,
@@ -39,12 +44,11 @@ async function AnnoucementPosts() {
   }
   return posts;
 }
-var data = await AnnoucementPosts()
 
 const sanitizeHtml = (html: any) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, "text/html");
-  
+
   // Remove all <img> tags
   const images = doc.querySelectorAll("img");
   images.forEach((img) => img.remove());
@@ -53,27 +57,69 @@ const sanitizeHtml = (html: any) => {
 };
 
 const Annoucements = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const posts = await AnnoucementPosts();
+      setData(posts);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        {[...Array(2)].map((_, index) => (
+          <div
+            key={index}
+            className="p-2 mt-2 mb-4 animate-pulse  rounded"
+          >
+            <div className="h-6 bg-gray-300 dark:bg-secondary rounded w-3/4 mb-2"></div>
+            <div className="h-4 bg-gray-300 dark:bg-secondary rounded w-full mb-1"></div>
+            <div className="h-4 bg-gray-300 dark:bg-secondary rounded w-5/6"></div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
-      <>
-      {data.splice(0,2).map((post) => (
-          <div className="p-2 mt-2 mb-4 hover:bg-white dark:hover:bg-black hover:brightness-[95%] animate-entry" key={post.slug}>
-          <a href={'https://forum.vatsim-scandinavia.org/d/'+post.slug} target="_blank">
-              <div className="flex">
-                  <div className="w-[90%]">
-                      <div className="font-semibold text-lg md:text-xl text-secondary dark:text-white">
-                          {post.title}
-                      </div>
-                      <div className="text-sm line-clamp-2" dangerouslySetInnerHTML={{__html: sanitizeHtml(post.content)}}>
-
-                      </div>
-                  </div>
-                  <div className="text-grey text-right text-md dark:text-gray-300 font-bold w-[10%]">
-                      {((new Date(post.created).toLocaleDateString('default', { day: "2-digit",month: 'short' })).toUpperCase())}
-                  </div>
+    <>
+      {data.splice(0, 2).map((post) => (
+        <div
+          className="p-2 mt-2 mb-4 hover:bg-white dark:hover:bg-black hover:brightness-[95%] animate-entry"
+          key={post.slug}
+        >
+          <a
+            href={"https://forum.vatsim-scandinavia.org/d/" + post.slug}
+            target="_blank"
+          >
+            <div className="flex">
+              <div className="w-[90%]">
+                <div className="font-semibold text-lg md:text-xl text-secondary dark:text-white">
+                  {post.title}
+                </div>
+                <div
+                  className="text-sm line-clamp-2"
+                  dangerouslySetInnerHTML={{
+                    __html: sanitizeHtml(post.content),
+                  }}
+                ></div>
               </div>
+              <div className="text-grey text-right text-md dark:text-gray-300 font-bold w-[10%]">
+                {new Date(post.created)
+                  .toLocaleDateString("default", {
+                    day: "2-digit",
+                    month: "short",
+                  })
+                  .toUpperCase()}
+              </div>
+            </div>
           </a>
-      </div>
+        </div>
       ))}
     </>
   );

9221ab3a0d720675b6b9d6dbf35fc6d34b24ffd2
Simon Bjerre <bjerrecs@protonmail.com>
2025-07-25 08:02:57 +0200

    updated es module of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ["class"],
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
@@ -92,5 +94,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindcssAnimate],
 }

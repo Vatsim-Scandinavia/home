dee5f4daedf99621f7f0cd190656984adcd24983
simon Bjerre <bjerrecs@protonmail.com>
2026-04-12 22:38:29 +0200

    fix: alt tags

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -85,13 +85,13 @@ const Events = () => {
         <div className="flex flex-col w-full h-fit" id="live-stats" style={{ display: loading ? 'none' : 'flex' }}>
             <div className="flex h-full flex-col gap-2" >
                 {events.slice(0, 2).map((item, index) => (
-                    <a href={item.url} target='_blank' key={item.id || index} className='aspect-video h-1/3 md:h-60 flex dark:hover:!text-primary text-secondary dark:text-white hover:bg-snow transition-all p-2 rounded'>
+                    <a href={item.url} target='_blank' rel='noopener noreferrer' aria-label={`View event: ${item.name}`} key={item.id || index} className='aspect-video h-1/3 md:h-60 flex dark:hover:!text-primary text-secondary dark:text-white hover:bg-snow transition-all p-2 rounded'>
 
-                        <img aria-label={`View event: ${item.name}`} className='h-full aspect-video bg-center bg-cover rounded' src={item.banner}/>
+                        <img alt={`Event banner for ${item.name}`} className='h-full aspect-video bg-center bg-cover rounded' src={item.banner}/>
 
                         <div className='w-full h-full px-2 hidden md:flex flex-col gap-2 relative'>
                             <h2 className='font-bold text-xl md:text-2xl'>{item.name}</h2>
-                            <p className='text-grey font-bold dark:text-gray-300 -mt-2 mb-2'>{events.length !== 0 ? dateConverter(item.start_datetime, item.end_datetime) : ""}z</p>
+                            <p className='text-grey font-bold dark:text-gray-300 -mt-2 mb-2'>{events.length !== 0 ? dateConverter(item.start_datetime, item.end_datetime) : ""}</p>
                             <p className={`line-clamp-6 mb-1 text-black dark:text-white`}>{item.short_description}</p>
                         </div>
                     </a>
@@ -99,11 +99,12 @@ const Events = () => {
                 <div className="navigation-wrapper h-1/3 m-2">
                     <div ref={sliderRef} className="keen-slider">
                         {events.slice(2, 9).map((item, index) => (
-                            <a key={item.id || index} style={{ '--image-url': `url(${item.banner})` }} aria-label={`View event: ${item.name}`} className={`keen-slider__slide bg-gray-800 bg-[image:var(--image-url)] bg-cover inline-block number-slide${index} rounded aspect-video`} target='_blank' href={item.url} />
+                            <a key={item.id || index} style={{ '--image-url': `url(${item.banner})` }} aria-label={`View event: ${item.name}`} className={`keen-slider__slide bg-gray-800 bg-[image:var(--image-url)] bg-cover inline-block number-slide${index} rounded aspect-video`} target='_blank' rel='noopener noreferrer' href={item.url} />
                         ))}
                         <a
                             href="https://events.vatsim-scandinavia.org"
                             target="_blank"
+                            rel="noopener noreferrer"
                             className="keen-slider__slide w-12 h-auto aspect-video bg-secondary text-white hover:brightness-[95%] rounded flex items-center justify-center text-center font-semibold"
                         >
                             More Events <ExternalLinkIcon width="0.75rem" marginLeft="0.3rem" />

92ae6c3e84bd27fe78d8c452f768ba1c2c29426d
simon Bjerre <bjerrecs@protonmail.com>
2026-04-13 16:53:20 +0200

    fix: handle 404 on server-island

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
     output: "server",
